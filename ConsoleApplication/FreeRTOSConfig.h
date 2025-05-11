de5513d5b77979255075564414cad437f3a0e277
Danh Phan <phandanh2112@gmail.com>
2025-05-11 11:57:26 +0700

    v1.1.0: add demos + log

--- ConsoleApplication/FreeRTOSConfig.h
+++ ConsoleApplication/FreeRTOSConfig.h
@@ -82,14 +82,14 @@
 /* Set configUSE_PREEMPTION to 1 to use pre-emptive scheduling.  Set
  * configUSE_PREEMPTION to 0 to use co-operative scheduling.
  * See https://www.freertos.org/single-core-amp-smp-rtos-scheduling.html. */
-#define configUSE_PREEMPTION                       0
+#define configUSE_PREEMPTION                       1
 
 /* Set configUSE_TIME_SLICING to 1 to have the scheduler switch between Ready
  * state tasks of equal priority on every tick interrupt.  Set
  * configUSE_TIME_SLICING to 0 to prevent the scheduler switching between Ready
  * state tasks just because there was a tick interrupt.  See
  * https://freertos.org/single-core-amp-smp-rtos-scheduling.html. */
-#define configUSE_TIME_SLICING                     0
+#define configUSE_TIME_SLICING                     1
 
 /* Set configUSE_PORT_OPTIMISED_TASK_SELECTION to 1 to select the next task to
  * run using an algorithm optimised to the instruction set of the target
@@ -153,7 +153,7 @@
 /* configQUEUE_REGISTRY_SIZE sets the maximum number of queues and semaphores
  * that can be referenced from the queue registry.  Only required when using a
  * kernel aware debugger.  Defaults to 0 if left undefined. */
-#define configQUEUE_REGISTRY_SIZE                  0
+#define configQUEUE_REGISTRY_SIZE                  10
 
 /* Set configENABLE_BACKWARD_COMPATIBILITY to 1 to map function names and
  * datatypes from old version of FreeRTOS to their latest equivalent.  Defaults
@@ -636,7 +636,7 @@
 #define configUSE_MUTEXES                      1
 #define configUSE_RECURSIVE_MUTEXES            1
 #define configUSE_COUNTING_SEMAPHORES          1
-#define configUSE_QUEUE_SETS                   0
+#define configUSE_QUEUE_SETS                   1
 #define configUSE_APPLICATION_TASK_TAG         0
 
 /* USE_POSIX_ERRNO enables the task global FreeRTOS_errno variable which will
--- README.md
+++ README.md
@@ -39,7 +39,9 @@
 
 ### Source
 
-- ConsoleApplication: sample application
+- ConsoleApplication: workspace of sample application
+- ConsoleApplication/Demo: sample to run (*.log -> copy to main.c)
+- ConsoleApplication/Log: log output
 - FreeRTOS-Kernel: FreeRTOS kernel implementation
 - copy_specific-files: script to copy source code from **FreeRTOS-Kernel** to **ConsoleApplication**
 
@@ -49,5 +51,5 @@
 2. In **FreeRTOS-Kernel**/, you can checkout any tags (version of FreeRTOS kernel) for trial. I'm using version 11.2.0
 3. In /**copy_specific-files**/, modify **filelist.txt** to include necessary source files. Then execute **cp_multiFiles.bat** to copy to **ConsoleApplication**.
 4. In **ConsoleApplication**/, open **ConsoleApplication.sln** to go to simulation environment
-5. On right side of IDE, in **Resource Files**, you can copy content in each demo file (*.txt) and paste to **main.c** in **Source Files**
+5. On right side of IDE, in **Resource Files**, you can copy content in each demo file (*.log) and paste to **main.c** in **Source Files**
 6. Finally, you can build solution and run the simulation
